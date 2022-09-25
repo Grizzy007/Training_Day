639aef07d2322da6b9fd5efc60358c06417aa993
Стас <stas.ruzhytskiy@gamil.com>
2022-09-25 13:56:41 +0300

    fix

--- src/main/java/ua/nure/tkp/trainingday/TrainingDayApplication.java
+++ src/main/java/ua/nure/tkp/trainingday/TrainingDayApplication.java
@@ -1,6 +1,5 @@
 package ua.nure.tkp.trainingday;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrainingDayApplication {
 
     public static void main(String[] args) {
-//        Flyway flyway = Flyway.configure()
-//                .dataSource("jdbc:mysql://localhost/training_day", "root", "Agent8998Agent_007")
-//                .baselineOnMigrate(true)
-//                .locations("db/migration")
-//                .load();
-//        flyway.migrate();
+
         SpringApplication.run(TrainingDayApplication.class, args);
     }
 
