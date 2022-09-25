756df8cd08feeed1d02f7231d49b4293956f9032
Стас <stas.ruzhytskiy@gamil.com>
2022-09-25 13:58:24 +0300

    fix

--- src/main/java/ua/nure/tkp/trainingday/TrainingDayApplication.java
+++ src/main/java/ua/nure/tkp/trainingday/TrainingDayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrainingDayApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(TrainingDayApplication.class, args);
     }
 
